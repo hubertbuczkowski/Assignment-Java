90106e1979d48caaab5bfa34bd4b12b3b4f6d2d5
hubertbuczkowski <h_buczkowski@interia.eu>
2016-04-10 21:15:39 +0100

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,9 +1,46 @@
 
-public class Menu {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+public class Menu 
+{
+
+	static public List<String> TextList = new List<String>();
+	
+	
+	
+	public static void main(String[] args)throws FileNotFoundException, NullPointerException
+	{
+		Words wrds = new Words("words.csv");
+		Scanner inputStream = new Scanner (new File("Blood.txt"));
+		
+		String[] Text;
+			
+		while(inputStream.hasNext())
+		{
+			String word = inputStream.next();
+
+			word = word.replace(" ", "");
+			word = word.replace(".", "");
+			word = word.replace(",", "");
+			word = word.replace("\"", "");
+			
+			Text.append("");
+			
+			if((TextList == null ||TextList.contains(word)== false) && wrds.WordCheck(word) == false)
+			{
+				TextList.add(word);
+				System.out.println(word);
+			}
+		}
+		
+        inputStream.close();
 	}
 
 }
+
+//&& Words.WordCheck(word) == false
