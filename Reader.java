ff7fdc68cbdb85ed62e26ae49ee71c70cef27c66
hubertbuczkowski <h_buczkowski@interia.eu>
2016-04-11 10:55:05 +0100

    Update Reader.java

--- Reader.java
+++ Reader.java
@@ -1,28 +1,52 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
 public class Reader 
 {
-	static List<String> TextList;
+	static List<String> TextList = new ArrayList<String>();
+	static List<Integer> WordAmount = new ArrayList<Integer>();
+	static int pointer;
 	
 	public Reader(String Address) throws FileNotFoundException
 	{
-		
+		int i;
 		Scanner inputStream = new Scanner (new File("Blood.txt"));
+		
 		
 		while(inputStream.hasNext())
 		{
 			String word = inputStream.next();
-			if(TextList.contains(word) == false && Words.WordCheck(word) == false)
+
+			word = word.replace(" ", "");
+			word = word.replace(".", "");
+			word = word.replace(",", "");
+			word = word.replace("\"", "");
+			word = word.replace("-", "");
+			word = word.toLowerCase();
+			
+			
+			if((TextList == null ||TextList.contains(word)== false) && Words.WordCheck(word) == false)
 			{
 				TextList.add(word);
+				WordAmount.add(1);
+			}
+			if(TextList.contains(word) == true)
+			{
+				WordAmount.set(TextList.indexOf(word), WordAmount.get(TextList.indexOf(word)) + 1);
 			}
 		}
 		
+		for(i=0; i < 10; i++)
+		{
+			pointer = WordAmount.indexOf(Collections.max(WordAmount));
+			System.out.println(TextList.get(pointer) + " - " + WordAmount.get(pointer));
+			WordAmount.set(pointer, 0);
+		}
+		
         inputStream.close();
-
-
 	}
 }
